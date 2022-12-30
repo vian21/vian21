04d89566101ddc6df597bf6bc9c8769afc46cabb
Aaron Tshibangu Mulangu <2684620@collegelacite.ca>
2022-12-30 15:18:58 -0500

    test

--- src/pages/commands.ts
+++ src/pages/commands.ts
@@ -1,6 +1,6 @@
 export const commands: Record<string, string> = {
     about: `
-    # Hello World!\nMy name is Patrick Igiraneza, I am an enthusiastic Software/Web developer. I am originally from Burundi which is a country found in the east of Africa. I am currently enrolled at the university of Ottawa in Software engineering(CO-OP) in French Immersion.
+    # Hello World!\nMy name is Aaron Th, I am an enthusiastic Software/Web developer. I am originally from Burundi which is a country found in the east of Africa. I am currently enrolled at the university of Ottawa in Software engineering(CO-OP) in French Immersion.
     
     **Location** : Gatineau, Canada
 
